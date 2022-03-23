ede876754816a9e370b130f560d89e256abc49a4
Dragoș Străinu <str.dr4605@gmail.com>
2022-03-23 10:44:01 +0200

    feat: MVP, rename to why

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# why-run
+# npx-why
 
 A tool to document you package.json scripts
 
 ```bash
-npx why-run --init # will create a package-why.json file where you can write docs for your scripts
-npx why-run test # will display docs for 'npm run test' command
+npx why --init # will create a package-why.json file where you can write docs for your scripts
+npx why test # will display docs for 'npm run test' command
 ```
--- src/index.js
+++ src/index.js
@@ -9,9 +9,38 @@ const packageObj = JSON.parse(package);
 const packageWhyPath = path.join(process.cwd(), "./package-why.json");
 
 const arg = process.argv[2];
+if (!arg) {
+  if (!fs.existsSync(packageWhyPath)) {
+    console.log("package-why.json does not exist, please run npx why --init");
+    return;
+  }
+
+  const packageWhy = fs.readFileSync(packageWhyPath);
+  const whyObj = JSON.parse(packageWhy);
+  const packageScripts = packageObj["scripts"];
+  const whyScriptsDesc = whyObj["scripts"];
+
+  for (const scriptName of Object.keys(packageScripts)) {
+    const desc = whyScriptsDesc[scriptName];
+    const packageScript = packageScripts[scriptName];
+
+    console.log(`\n\x1b[32m${scriptName}\x1b[0m:`);
+    console.log(
+      "\x1b[33m%s\x1b[0m",
+      `\n  ${
+        desc ||
+        `Description for '${scriptName}' not found in package-why.json, please ask maintainer to add it`
+      }\n`
+    );
+    console.log(`  script: \x1b[32m${packageScript}\x1b[0m`);
+    console.log(`  try it: \x1b[35mnpm run ${scriptName}\x1b[0m`);
+  }
+  return;
+}
+
 if (arg === "--init") {
   if (fs.existsSync(packageWhyPath)) {
-    console.log("package-why.json exists, run npx why-run <somescript>");
+    console.log("package-why.json exists, run npx why <somescript>");
     return;
   }
   const newPackageWhyObj = {
@@ -36,10 +65,17 @@ const scriptName = arg;
 
 const packageWhy = fs.readFileSync(packageWhyPath);
 
-const obj = JSON.parse(packageWhy);
+const whyObj = JSON.parse(packageWhy);
 
 const packageScript = packageObj["scripts"][scriptName];
-const desc = obj["scripts"][scriptName];
+if (!packageScript) {
+  console.log(
+    "\x1b[31m%s\x1b[0m",
+    `script '${scriptName}' not found in package.json`
+  );
+  return;
+}
+const desc = whyObj["scripts"][scriptName];
 
 console.log(
   "\x1b[33m%s\x1b[0m",
